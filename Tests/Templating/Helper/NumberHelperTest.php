8471b35c97260955aa65640f8b13b2069ff31312
Matthieu Moquet <matthieu@moquet.net>
2011-07-01 21:31:54 +0200

    [Tests] Corrected float value

--- Tests/Templating/Helper/NumberHelperTest.php
+++ Tests/Templating/Helper/NumberHelperTest.php
@@ -15,8 +15,8 @@ class NumberHelperTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals($this->helper->convert(123456789, 'k', 1000), 123456.789);
         $this->assertEquals($this->helper->convert(123456789, 'M', 1000), 123.456789);
-        $this->assertEquals($this->helper->convert(123456789, 'k', 1024), 120563.2705);
-        $this->assertEquals($this->helper->convert(123456789, 'M', 1024), 117.7375689);
+        $this->assertEquals($this->helper->convert(123456789, 'k', 1024), 123456789/1024);
+        $this->assertEquals($this->helper->convert(123456789, 'M', 1024), 123456789/(1024*1024));
     }
     
     public function testReadable()
